3617d83e40ce20563b87f4d783d597e3ae2db201
Gerald Haesendonck <gerald.haesendonck@ugent.be>
2014-11-20 13:38:49 +0100

    Print help when wrong option is given.

--- src/org/linkeddatafragments/standalone/JettyServer.java
+++ src/org/linkeddatafragments/standalone/JettyServer.java
@@ -53,6 +53,9 @@ public class JettyServer {
 			// The use of server.join() the will make the current thread join and wait until the server is done executing.
 			// See http://docs.oracle.com/javase/7/docs/api/java/lang/Thread.html#join()
 			server.join();
+		} catch (ParseException e) {
+			System.out.println(e.getMessage());
+			printHelp = true;
 
 		} finally {
 			if (printHelp) {
