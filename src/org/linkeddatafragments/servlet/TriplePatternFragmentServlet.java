605b15f8d9a804447909576469faff1046055aec
Bart Hanssens <bart.hanssens@fedict.be>
2015-11-30 14:40:36 +0100

    Refactoring for readability, using more final static/constants

--- src/org/linkeddatafragments/servlet/TriplePatternFragmentServlet.java
+++ src/org/linkeddatafragments/servlet/TriplePatternFragmentServlet.java
@@ -1,8 +1,11 @@
 package org.linkeddatafragments.servlet;
 
 import com.google.gson.JsonObject;
+
 import java.io.File;
 import java.io.FileReader;
+import java.io.IOException;
+import java.net.URISyntaxException;
 import java.util.HashMap;
 import java.util.Map.Entry;
 import java.util.regex.Matcher;
@@ -14,12 +17,15 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.http.HttpHeaders;
 import org.apache.http.client.utils.URIBuilder;
+
 import org.linkeddatafragments.config.ConfigReader;
 import org.linkeddatafragments.datasource.TriplePatternFragment;
 import org.linkeddatafragments.datasource.IDataSource;
-
-import static org.linkeddatafragments.util.CommonResources.*;
+import org.linkeddatafragments.datasource.DataSourceFactory;
+import org.linkeddatafragments.exceptions.DataSourceException;
+import org.linkeddatafragments.util.CommonResources;
 
 import com.hp.hpl.jena.datatypes.TypeMapper;
 import com.hp.hpl.jena.datatypes.xsd.XSDDatatype;
@@ -30,139 +36,235 @@ import com.hp.hpl.jena.rdf.model.RDFNode;
 import com.hp.hpl.jena.rdf.model.Resource;
 import com.hp.hpl.jena.rdf.model.ResourceFactory;
 import com.hp.hpl.jena.shared.InvalidPropertyURIException;
-import org.linkeddatafragments.datasource.DataSourceFactory;
+
+import org.apache.commons.codec.CharEncoding;
 
 /**
  * Servlet that responds with a Basic Linked Data Fragment.
  *
  * @author Ruben Verborgh
+ * @author Bart Hanssens
  */
 public class TriplePatternFragmentServlet extends HttpServlet {
-
     private final static long serialVersionUID = 1L;
-    private final static Pattern STRINGPATTERN = Pattern.compile("^\"(.*)\"(?:@(.*)|\\^\\^<?([^<>]*)>?)?$");
-    private final static TypeMapper types = TypeMapper.getInstance();
+    
+    // Parameters
+    public final static String CFGFILE = "configFile";
+    public final static String SUBJ = "subject";
+    public final static String PRED = "predicate";
+    public final static String OBJ = "object";
+    public final static String PAGE = "page";
+    
+    
+    private final static Pattern STRINGPATTERN = 
+                    Pattern.compile("^\"(.*)\"(?:@(.*)|\\^\\^<?([^<>]*)>?)?$");
+    private final static TypeMapper TYPES = TypeMapper.getInstance();
     private final static long TRIPLESPERPAGE = 100;
 
     private ConfigReader config;
     private final HashMap<String, IDataSource> dataSources = new HashMap<>();
 
+    
+    private File getConfigFile(ServletConfig config) throws IOException {
+        String path = config.getServletContext().getRealPath("/");
+        if (path == null) {
+            // this can happen when running standalone
+            path = System.getProperty("user.dir");
+        }
+        File cfg = new File(path, "config-example.json");
+        if (config.getInitParameter(CFGFILE) != null) {
+            cfg = new File(config.getInitParameter(CFGFILE));
+        }
+        if (!cfg.exists()) {
+            throw new IOException("Configuration file " + cfg + " not found.");
+        }
+        if (!cfg.isFile()) {
+            throw new IOException("Configuration file " + cfg + " is not a file.");
+        }
+        return cfg;
+    }
+    
     @Override
     public void init(ServletConfig servletConfig) throws ServletException {
         try {
-            // find the configuration file
-            String applicationPathStr = servletConfig.getServletContext().getRealPath("/");
-            if (applicationPathStr == null) {	// this can happen when running standalone
-                applicationPathStr = System.getProperty("user.dir");
-            }
-            final File applicationPath = new File(applicationPathStr);
-
-            File configFile = new File(applicationPath, "config-example.json");
-            if (servletConfig.getInitParameter("configFile") != null) {
-                configFile = new File(servletConfig.getInitParameter("configFile"));
-            }
-
-            if (!configFile.exists()) {
-                throw new Exception("Configuration file " + configFile + " not found.");
-            }
-
-            if (!configFile.isFile()) {
-                throw new Exception("Configuration file " + configFile + " is not a file.");
-            }
-
             // load the configuration
+            File configFile = getConfigFile(servletConfig);
             config = new ConfigReader(new FileReader(configFile));
+            
             for (Entry<String, JsonObject> dataSource : config.getDataSources().entrySet()) {
                 dataSources.put(dataSource.getKey(), DataSourceFactory.create(dataSource.getValue()));
             }
-        } catch (Exception e) {
+        } catch (IOException | DataSourceException e) {
             throw new ServletException(e);
         }
     }
 
+    /**
+     * Get the datasource
+     * 
+     * @param request
+     * @return
+     * @throws IOException 
+     */
+    private IDataSource getDataSource(HttpServletRequest request) throws IOException {
+        String contextPath = request.getContextPath();
+        String requestURI = request.getRequestURI();
+
+        String path = contextPath == null 
+                                ? requestURI 
+                                : requestURI.substring(contextPath.length());
+        String dataSourceName = path.substring(1);
+        IDataSource dataSource = dataSources.get(dataSourceName);
+        if (dataSource == null) {
+            throw new IOException("Data source not found.");
+        }
+        return dataSource;
+    }
+    
+    /**
+     * Get dataset url
+     * 
+     * @param request
+     * @return 
+     */
+    private String getDatasetUrl(HttpServletRequest request) {
+        String hostName = request.getHeader(HttpHeaders.SERVER);
+        if (hostName == null) {
+            hostName = request.getServerName();
+        }
+        return request.getScheme() + "://" + hostName + request.getRequestURI();
+    }
+    
+    /**
+     * Add total and limit
+     * 
+     * @param output
+     * @param fragmentId
+     * @param total
+     * @param limit 
+     */
+    private void addMeta(Model output, Resource datasetId, Resource fragmentId, 
+                                                        long total, long limit) {
+        output.add(datasetId, CommonResources.RDF_TYPE, CommonResources.VOID_DATASET);
+        output.add(datasetId, CommonResources.RDF_TYPE, CommonResources.HYDRA_COLLECTION);
+        output.add(datasetId, CommonResources.VOID_SUBSET, fragmentId);
+            
+        output.add(fragmentId, CommonResources.RDF_TYPE, CommonResources.HYDRA_COLLECTION);
+        output.add(fragmentId, CommonResources.RDF_TYPE, CommonResources.HYDRA_PAGEDCOLLECTION);
+        
+        Literal totalTyped = output.createTypedLiteral(total, XSDDatatype.XSDinteger);
+        Literal limitTyped = output.createTypedLiteral(limit, XSDDatatype.XSDinteger);
+
+        output.add(fragmentId, CommonResources.VOID_TRIPLES, totalTyped);
+        output.add(fragmentId, CommonResources.HYDRA_TOTALITEMS, totalTyped);
+        output.add(fragmentId, CommonResources.HYDRA_ITEMSPERPAGE, limitTyped);
+    }
+    
+
+    /**
+     * Add reference to first/previous/next page
+     * 
+     * @param output
+     * @param fragmentId
+     * @param fragmentUrl
+     * @param total
+     * @param limit
+     * @param offset
+     * @param page
+     * @throws URISyntaxException 
+     */
+    private void addPages(Model output, Resource fragmentId, String fragmentUrl, 
+                long total, long limit, long offset, long page) throws URISyntaxException { 
+        URIBuilder pagedUrl = new URIBuilder(fragmentUrl);
+        
+        pagedUrl.setParameter(PAGE, "1");
+        output.add(fragmentId, CommonResources.HYDRA_FIRSTPAGE, 
+                                    output.createResource(pagedUrl.toString()));
+        if (offset > 0) {
+            pagedUrl.setParameter(PAGE, Long.toString(page - 1));
+            output.add(fragmentId, CommonResources.HYDRA_PREVIOUSPAGE, 
+                                    output.createResource(pagedUrl.toString()));
+        }
+        if (offset + limit < total) {
+            pagedUrl.setParameter(PAGE, Long.toString(page + 1));
+            output.add(fragmentId, CommonResources.HYDRA_NEXTPAGE, 
+                                    output.createResource(pagedUrl.toString()));
+        }
+    }
+     
+    /**
+     * Add controls to output
+     * 
+     * @param output
+     * @param datasetId
+     * @param datasetUrl 
+     */
+    private void addControls(Model output, Resource datasetId, String datasetUrl) {
+        // add controls
+        Resource triplePattern = output.createResource();
+        Resource subjectMapping = output.createResource();
+        Resource predicateMapping = output.createResource();
+        Resource objectMapping = output.createResource();
+
+        output.add(datasetId, CommonResources.HYDRA_SEARCH, triplePattern);
+        output.add(triplePattern, CommonResources.HYDRA_TEMPLATE, output.createLiteral(datasetUrl + "{?subject,predicate,object}"));
+        output.add(triplePattern, CommonResources.HYDRA_MAPPING, subjectMapping);
+        output.add(triplePattern, CommonResources.HYDRA_MAPPING, predicateMapping);
+        output.add(triplePattern, CommonResources.HYDRA_MAPPING, objectMapping);
+        
+        output.add(subjectMapping, CommonResources.HYDRA_VARIABLE, output.createLiteral(SUBJ));
+        output.add(subjectMapping, CommonResources.HYDRA_PROPERTY, CommonResources.RDF_SUBJECT);
+        
+        output.add(predicateMapping, CommonResources.HYDRA_VARIABLE, output.createLiteral(PRED));
+        output.add(predicateMapping, CommonResources.HYDRA_PROPERTY, CommonResources.RDF_PREDICATE);
+        output.add(objectMapping, CommonResources.HYDRA_VARIABLE, output.createLiteral(OBJ));
+        
+        output.add(objectMapping, CommonResources.HYDRA_PROPERTY, CommonResources.RDF_OBJECT);
+    }
+    
+    
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException {
         try {
-            // find the data source
-            String contextPath = request.getContextPath();
-            String requestURI = request.getRequestURI();
-            String path = contextPath == null ? requestURI : requestURI.substring(contextPath.length());
-            String query = request.getQueryString();
-            String dataSourceName = path.substring(1);
-            IDataSource dataSource = dataSources.get(dataSourceName);
-            if (dataSource == null) {
-                throw new Exception("Data source not found.");
-            }
-
+            IDataSource dataSource = getDataSource(request);
+            
             // query the fragment
-            Resource subject = parseAsResource(request.getParameter("subject"));
-            Property predicate = parseAsProperty(request.getParameter("predicate"));
-            RDFNode object = parseAsNode(request.getParameter("object"));
-            long page = Math.max(1, parseAsInteger(request.getParameter("page")));
-            long limit = TRIPLESPERPAGE, offset = limit * (page - 1);
-            TriplePatternFragment fragment = dataSource.getFragment(subject, predicate, object, offset, limit);
+            Resource subject = parseAsResource(request.getParameter(SUBJ));
+            Property predicate = parseAsProperty(request.getParameter(PRED));
+            RDFNode object = parseAsNode(request.getParameter(OBJ));
+            
+            long page = Math.max(1, parseAsInteger(request.getParameter(PAGE)));
+            long limit = TRIPLESPERPAGE;
+            long offset = limit * (page - 1);
+        
+            TriplePatternFragment fragment = 
+                    dataSource.getFragment(subject, predicate, object, offset, limit);
 
             // fill the output model
             Model output = fragment.getTriples();
             output.setNsPrefixes(config.getPrefixes());
 
             // add dataset metadata
-            String hostName = request.getHeader("Host");
-            String datasetUrl = request.getScheme() + "://"
-                    + (hostName == null ? request.getServerName() : hostName) + request.getRequestURI();
-            String fragmentUrl = query == null ? datasetUrl : (datasetUrl + "?" + query);
+            String datasetUrl = getDatasetUrl(request);
             Resource datasetId = output.createResource(datasetUrl + "#dataset");
+            
+            String query = request.getQueryString();
+            String fragmentUrl = query == null ? datasetUrl : (datasetUrl + "?" + query);
             Resource fragmentId = output.createResource(fragmentUrl);
+
+            long total = fragment.getTotalSize();
             
-            output.add(datasetId, RDF_TYPE, VOID_DATASET);
-            output.add(datasetId, RDF_TYPE, HYDRA_COLLECTION);
-            output.add(datasetId, VOID_SUBSET, fragmentId);
-
-            // add fragment metadata
-            output.add(fragmentId, RDF_TYPE, HYDRA_COLLECTION);
-            output.add(fragmentId, RDF_TYPE, HYDRA_PAGEDCOLLECTION);
-            Literal total = output.createTypedLiteral(fragment.getTotalSize(), XSDDatatype.XSDinteger);
-            output.add(fragmentId, VOID_TRIPLES, total);
-            output.add(fragmentId, HYDRA_TOTALITEMS, total);
-            output.add(fragmentId, HYDRA_ITEMSPERPAGE, output.createTypedLiteral(limit, XSDDatatype.XSDinteger));
-
-            // add pages
-            final URIBuilder pagedUrl = new URIBuilder(fragmentUrl);
-            pagedUrl.setParameter("page", "1");
-            output.add(fragmentId, HYDRA_FIRSTPAGE, output.createResource(pagedUrl.toString()));
-            if (offset > 0) {
-                pagedUrl.setParameter("page", Long.toString(page - 1));
-                output.add(fragmentId, HYDRA_PREVIOUSPAGE, output.createResource(pagedUrl.toString()));
-            }
-            if (offset + limit < fragment.getTotalSize()) {
-                pagedUrl.setParameter("page", Long.toString(page + 1));
-                output.add(fragmentId, HYDRA_NEXTPAGE, output.createResource(pagedUrl.toString()));
-            }
-
-            // add controls
-            Resource triplePattern = output.createResource();
-            Resource subjectMapping = output.createResource();
-            Resource predicateMapping = output.createResource();
-            Resource objectMapping = output.createResource();
+            addMeta(output, datasetId, fragmentId, total, limit);
+            addPages(output, fragmentId, fragmentUrl, total, limit, offset, page);           
+            addControls(output, datasetId, datasetUrl);
             
-            output.add(datasetId, HYDRA_SEARCH, triplePattern);
-            output.add(triplePattern, HYDRA_TEMPLATE, output.createLiteral(datasetUrl + "{?subject,predicate,object}"));
-            output.add(triplePattern, HYDRA_MAPPING, subjectMapping);
-            output.add(triplePattern, HYDRA_MAPPING, predicateMapping);
-            output.add(triplePattern, HYDRA_MAPPING, objectMapping);
-            output.add(subjectMapping, HYDRA_VARIABLE, output.createLiteral("subject"));
-            output.add(subjectMapping, HYDRA_PROPERTY, RDF_SUBJECT);
-            output.add(predicateMapping, HYDRA_VARIABLE, output.createLiteral("predicate"));
-            output.add(predicateMapping, HYDRA_PROPERTY, RDF_PREDICATE);
-            output.add(objectMapping, HYDRA_VARIABLE, output.createLiteral("object"));
-            output.add(objectMapping, HYDRA_PROPERTY, RDF_OBJECT);
-
             // serialize the output as Turtle
-            response.setHeader("Server", "Linked Data Fragments Server");
+            response.setHeader(HttpHeaders.SERVER, "Linked Data Fragments Server");
             response.setContentType("text/turtle");
-            response.setCharacterEncoding("utf-8");
+            response.setCharacterEncoding(CharEncoding.UTF_8);
+            
             output.write(response.getWriter(), "Turtle", fragmentUrl);
-        } catch (Exception e) {
+        } catch (IOException | URISyntaxException e) {
             throw new ServletException(e);
         }
     }
@@ -189,7 +291,9 @@ public class TriplePatternFragmentServlet extends HttpServlet {
      */
     private Resource parseAsResource(String value) {
         RDFNode subject = parseAsNode(value);
-        return subject == null || subject instanceof Resource ? (Resource) subject : INVALID_URI;
+        return subject == null || subject instanceof Resource 
+                ? (Resource) subject 
+                : CommonResources.INVALID_URI;
     }
 
     /**
@@ -204,10 +308,10 @@ public class TriplePatternFragmentServlet extends HttpServlet {
             try {
                 return ResourceFactory.createProperty(((Resource) predicateNode).getURI());
             } catch (InvalidPropertyURIException ex) {
-                return INVALID_URI;
+                return CommonResources.INVALID_URI;
             }
         }
-        return predicateNode == null ? null : INVALID_URI;
+        return predicateNode == null ? null : CommonResources.INVALID_URI;
     }
 
     /**
@@ -218,7 +322,7 @@ public class TriplePatternFragmentServlet extends HttpServlet {
      */
     private RDFNode parseAsNode(String value) {
         // nothing or empty indicates an unknown
-        if (value == null || value.length() == 0) {
+        if (value == null || value.isEmpty()) {
             return null;
         }
         // find the kind of entity based on the first character
@@ -242,11 +346,11 @@ public class TriplePatternFragmentServlet extends HttpServlet {
                         return ResourceFactory.createLangLiteral(body, lang);
                     }
                     if (type != null) {
-                        return ResourceFactory.createTypedLiteral(body, types.getSafeTypeByName(type));
+                        return ResourceFactory.createTypedLiteral(body, TYPES.getSafeTypeByName(type));
                     }
                     return ResourceFactory.createPlainLiteral(body);
                 }
-                return INVALID_URI;
+                return CommonResources.INVALID_URI;
             // assume it's a URI without angular brackets
             default:
                 return ResourceFactory.createResource(value);
--- src/org/linkeddatafragments/standalone/JettyServer.java
+++ src/org/linkeddatafragments/standalone/JettyServer.java
@@ -5,10 +5,11 @@ import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.DefaultParser;
 import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Options;
-import org.apache.commons.cli.ParseException;
+
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
+
 import org.linkeddatafragments.servlet.TriplePatternFragmentServlet;
 
 /**
@@ -25,61 +26,54 @@ import org.linkeddatafragments.servlet.TriplePatternFragmentServlet;
  *
  * @author Gerald Haesendonck
  * @author Miel Vander Sande
+ * @author Bart Hanssens
  */
 public class JettyServer {
-
+    private static void printHelp(Options options) {
+        HelpFormatter formatter = new HelpFormatter();
+        formatter.printHelp(JettyServer.class.getName() + " [config-example.json] [<options>]",
+                    "Starts a standalone LDF Triple Pattern server. Options:", options, "");
+    }
+    
     public static void main(String[] args) throws Exception {
         Options options = new Options();
         options.addOption("h", "help", false, "Print this help message and then exit.");
         options.addOption("p", "port", true, "The port the server listents to. The default is 8080.");
-        boolean printHelp = false;
         
         CommandLineParser parser = new DefaultParser();
-        try {
-            CommandLine commandLine = parser.parse(options, args);
+        CommandLine commandLine = parser.parse(options, args);
 
-            String config = null;
-            if (!commandLine.getArgList().isEmpty()) {
-                config = commandLine.getArgs()[0];
-            }
-
-            if (commandLine.hasOption('h')) {
-                printHelp = true;
-                return;
-            }
-            int port;
-            if (commandLine.hasOption('p')) {
-                port = Integer.parseInt(commandLine.getOptionValue('p'));
-            } else {
-                port = 8080;
-            }
-
-            // create a new (Jetty) server, and add a servlet handler
-            Server server = new Server(port);
-            ServletHandler handler = new ServletHandler();
-            server.setHandler(handler);
-
-            // add the TriplePatternFragmentsServlet to the handler
-            ServletHolder tpfServletHolder = new ServletHolder(new TriplePatternFragmentServlet());
-            tpfServletHolder.setInitParameter("configFile", config);
-            handler.addServletWithMapping(tpfServletHolder, "/*");
-
-            // start the server
-            server.start();
-            System.out.println("Started server, listening at port " + port);
-
-            // The use of server.join() the will make the current thread join and wait until the server is done executing.
-            // See http://docs.oracle.com/javase/7/docs/api/java/lang/Thread.html#join()
-            server.join();
-        } catch (ParseException e) {
-            System.out.println(e.getMessage());
-            printHelp = true;
-
-        } finally {
-            if (printHelp) {
-                HelpFormatter formatter = new HelpFormatter();
-                formatter.printHelp(JettyServer.class.getName() + " [config-example.json] [<options>]", "Starts a standalone LDF Trpile Pattern server. Options:", options, "");
-            }
+        String config = null;
+        if (!commandLine.getArgList().isEmpty()) {
+            config = commandLine.getArgs()[0];
         }
+
+        if (config == null || commandLine.hasOption('h')) {
+            printHelp(options);
+            System.exit(-1);
+        }    
+        
+        int port = 8080;
+        if (commandLine.hasOption('p')) {
+            port = Integer.parseInt(commandLine.getOptionValue('p'));
+        }
+
+        // create a new (Jetty) server, and add a servlet handler
+        Server server = new Server(port);
+        ServletHandler handler = new ServletHandler();
+        server.setHandler(handler);
+
+        // add the TriplePatternFragmentsServlet to the handler
+        ServletHolder tpfServletHolder = new ServletHolder(new TriplePatternFragmentServlet());
+        tpfServletHolder.setInitParameter(TriplePatternFragmentServlet.CFGFILE, config);
+        handler.addServletWithMapping(tpfServletHolder, "/*");
+
+        // start the server
+        server.start();
+        System.out.println("Started server, listening at port " + port);
+
+        // The use of server.join() the will make the current thread join and wait until the server is done executing.
+        // See http://docs.oracle.com/javase/7/docs/api/java/lang/Thread.html#join()
+        server.join();
     }
 }
