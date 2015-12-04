889dd0181910c4f178ff0288361f67b4529d1a19
Miel Vander Sande <miel.vandersande@ugent.be>
2015-12-04 15:52:01 +0100

    Added naive estimate

--- src/org/linkeddatafragments/datasource/JenaTDBDataSource.java
+++ src/org/linkeddatafragments/datasource/JenaTDBDataSource.java
@@ -8,8 +8,11 @@ import com.hp.hpl.jena.query.Query;
 import com.hp.hpl.jena.query.QueryExecution;
 import com.hp.hpl.jena.query.QueryExecutionFactory;
 import com.hp.hpl.jena.query.QueryFactory;
+import com.hp.hpl.jena.query.QuerySolution;
 import com.hp.hpl.jena.query.QuerySolutionMap;
+import com.hp.hpl.jena.query.ResultSet;
 import com.hp.hpl.jena.query.Syntax;
+import com.hp.hpl.jena.rdf.model.Literal;
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
 import com.hp.hpl.jena.rdf.model.Property;
@@ -20,21 +23,23 @@ import java.io.File;
 
 /**
  * Experimental Jena TDB-backed data source of Basic Linked Data Fragments.
- * 
+ *
  * @author Bart Hanssens <bart.hanssens@fedict.be>
  */
 public class JenaTDBDataSource extends DataSource {
     private final Dataset tdb;
-    private final String sparql = "CONSTRUCT WHERE { ?s ?p ?o } " + 
+    private final String sparql = "CONSTRUCT WHERE { ?s ?p ?o } " +
                                     "ORDER BY ?s ?p ?o";
-    
+
+    private final String count = "SELECT (COUNT(?s) AS ?count) WHERE { ?s ?p ?o }";
+
     private final Query query = QueryFactory.create(sparql, Syntax.syntaxSPARQL_11);
-    
+    private final Query countQuery = QueryFactory.create(count, Syntax.syntaxSPARQL_11);
 
     @Override
     public TriplePatternFragment getFragment(Resource subject, Property predicate, RDFNode object, long offset, long limit) {
         checkBoundaries(offset, limit);
-        
+
         Model model = tdb.getDefaultModel();
         QuerySolutionMap map = new QuerySolutionMap();
         if (subject != null) {
@@ -46,12 +51,12 @@ public class JenaTDBDataSource extends DataSource {
         if (object != null) {
             map.add("o", object);
         }
-        
+
         query.setOffset(offset);
         query.setLimit(limit);
-        
+
         Model triples = ModelFactory.createDefaultModel();
-        
+
         try (QueryExecution qexec = QueryExecutionFactory.create(query, model, map)) {
             qexec.execConstruct(triples);
         }
@@ -59,31 +64,39 @@ public class JenaTDBDataSource extends DataSource {
         if (triples.isEmpty()) {
             return new TriplePatternFragmentBase();
         }
-        
+
         // Try to get an estimate
         long size = triples.size();
         long estimate = -1;
 
-       
-        GraphStatisticsHandler stats = model.getGraph().getStatisticsHandler();
+        try (QueryExecution qexec = QueryExecutionFactory.create(countQuery, model, map)) {
+            ResultSet results = qexec.execSelect();
+            if (results.hasNext()) {
+                QuerySolution soln = results.nextSolution() ;
+                Literal literal = soln.getLiteral("count");
+                estimate = literal.getLong();
+            }
+        }
+
+        /*GraphStatisticsHandler stats = model.getGraph().getStatisticsHandler();
         if (stats != null) {
             Node s = (subject != null) ? subject.asNode() : null;
             Node p = (predicate != null) ? predicate.asNode() : null;
             Node o = (object != null) ? object.asNode() : null;
             estimate = stats.getStatistic(s, p, o);
-        } 
-        
+        }*/
+
         // No estimate or incorrect
         if (estimate < offset + size) {
             estimate = (size == limit) ? offset + size + 1 : offset + size;
         }
         return new TriplePatternFragmentBase(triples, estimate);
     }
-    
-    
+
+
     /**
      * Constructor
-     * 
+     *
      * @param title
      * @param description
      * @param tdbdir directory used for TDB backing
