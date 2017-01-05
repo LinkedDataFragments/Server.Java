24149ebe139b76918781116f2a51fff7b2c7b19a
Miel Vander Sande <miel.vandersande@ugent.be>
2017-01-05 09:56:58 +0100

    Moved GPG signing to deploy phase

--- src/main/java/org/linkeddatafragments/servlet/LinkedDataFragmentServlet.java
+++ src/main/java/org/linkeddatafragments/servlet/LinkedDataFragmentServlet.java
@@ -4,8 +4,6 @@ import com.google.gson.JsonObject;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
