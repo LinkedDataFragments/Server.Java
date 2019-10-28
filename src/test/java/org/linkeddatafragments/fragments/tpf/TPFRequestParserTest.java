aa15d306c6214c048eba47c05993e10e688fa3b7
Miel Vander Sande <miel.vandersande@ugent.be>
2019-10-28 10:45:07 +0100

    Added test files

---
+++ src/test/java/org/linkeddatafragments/fragments/tpf/TPFRequestParserTest.java
@@ -0,0 +1,40 @@
+package org.linkeddatafragments.fragments.tpf;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.linkeddatafragments.config.ConfigReader;
+import org.linkeddatafragments.fragments.tpf.TPFRequestParser.Worker;
+
+public class TPFRequestParserTest {
+
+	@Test
+	public void shouldReturnCorrectDatasetUrl() {
+		final TPFRequestParser parser = new TPFRequestParser(null);
+		final ServletContext servletContext = mock(ServletContext.class);
+		when(servletContext.getContextPath()).thenReturn("/foo");
+
+		final HttpServletRequest request = mock(HttpServletRequest.class);
+		when(request.getServletContext()).thenReturn(servletContext);
+		when(request.getServerPort()).thenReturn(123);
+		when(request.getScheme()).thenReturn("https");
+		when(request.getServerName()).thenReturn("foo.bla");
+		when(request.getServletPath()).thenReturn("");
+		when(request.getPathInfo()).thenReturn("/someDataset");
+		when(request.getRequestURI()).thenReturn("/foo" + "" + "/someDataset");
+
+		final ConfigReader config = mock(ConfigReader.class);
+		when(config.getBaseURL()).thenReturn(null);
+		
+		final Worker worker = parser.getWorker(request, config);
+		final String datasetUrl = worker.getDatasetURL();
+		Assert.assertEquals("wrong datasetUrl returned",
+				"https://foo.bla:123/foo/someDataset", datasetUrl);
+	}
+
+}
